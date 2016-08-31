562df7b714875aa31d167474a94dc24f3f31f3e7
Charming <OyCharming@gmail.com>
2016-08-31 16:44:43 +0800

    03. Decorator pattern example[lower case input stream].

--- src/main/java/com/github/stillivan/pattern/decorator/Test.java
+++ src/main/java/com/github/stillivan/pattern/decorator/Test.java
@@ -6,6 +6,12 @@ import com.github.stillivan.pattern.decorator.beverage.Espresso;
 import com.github.stillivan.pattern.decorator.condiment.Mocha;
 import com.github.stillivan.pattern.decorator.condiment.Soy;
 import com.github.stillivan.pattern.decorator.condiment.Whip;
+import com.github.stillivan.pattern.decorator.lowercase.LowerCaseInputStream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by charming on 16/8/31.
@@ -28,7 +34,25 @@ public class Test {
         System.out.println(beverage3.getDescription() + " $" + beverage3.cost());
     }
 
+    public static final void lowerCase() {
+        int c;
+        try {
+            InputStream in = new LowerCaseInputStream(new BufferedInputStream(
+                    new FileInputStream(Test.class.getResource("").getPath() + "test.txt")
+            ));
+
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        starbuzzCoffee();
+        //starbuzzCoffee();
+        lowerCase();
     }
 }
