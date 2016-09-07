45b0bfd69f46839bb6249a184e9db93e20e95a02
Charming <OyCharming@gmail.com>
2016-09-07 09:50:45 +0800

    08. Facade pattern example[home theater].

---
+++ src/main/java/com/github/stillivan/pattern/facade/Screen.java
@@ -0,0 +1,21 @@
+package com.github.stillivan.pattern.facade;
+
+/**
+ * @author Charming
+ * @since 2016-09-07 09:33
+ */
+public class Screen {
+    /**
+     * 把屏幕放下.
+     */
+    public void down() {
+        System.out.println("The screen is down.");
+    }
+
+    /**
+     * 把屏幕收起来.
+     */
+    public void up() {
+        System.out.println("The screen is down.");
+    }
+}
